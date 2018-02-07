2f9e42f7318f45358bc8fd2d578ce1651c015f83
CamTu <camtu@localhost.localdomain>
2018-02-07 15:21:13 +0700

    Correct the formula of Ec

--- pot-gau-mesh-optional/SAEPOT.f90
+++ pot-gau-mesh-optional/SAEPOT.f90
@@ -189,7 +189,9 @@ function corpot(i,j)
 !-- Vosko et al. 1980
     integer, intent(in) :: i, j
     real(dp) :: corpot
-    real(dp), parameter :: a = 0.0621814/2
+
+!-- Using ferromagnetic parameters
+    real(dp), parameter :: a = 0.0621814/2 
     real(dp), parameter :: b = 7.06042
     real(dp), parameter :: c = 18.057
     real(dp), parameter :: x0 = -0.32500
@@ -206,8 +208,9 @@ function corpot(i,j)
     Xx0 = x0**2 + b*x0 + c
     Q = sqrt(4*c - b**2)
 
+!-- In Hartree unit, the formula (in Rydberg unit) must be added factor 1/2
     ec = 0.5 * a * ( log(x**2 / Xx) + 2*b/Q * atan(Q / (2*x + b) ) &
-                   - b*x0 / Xx0 * (log( (x-x0)**2 / Xx ) + 2*(b + x*x0) /Q *atan( Q/(2*x + b) ) ) )
+                   - b*x0 / Xx0 * (log( (x-x0)**2 / Xx ) + 2*(b + 2*x0) /Q *atan( Q/(2*x + b) ) ) )
 
     corpot = ec - a/6 * ( (x - x0)*c - b*x*x0 ) / ( (x - x0) * Xx )
    
