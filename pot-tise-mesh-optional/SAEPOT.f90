eb4205c25ccd15725f7dbedc59066a464fda17f4
CamTu <camtu@localhost.localdomain>
2018-02-07 15:24:23 +0700

    Correct the formula of Ec

--- pot-tise-mesh-optional/SAEPOT.f90
+++ pot-tise-mesh-optional/SAEPOT.f90
@@ -5,6 +5,7 @@
 ! **********************************************************************
 
 program saepot_calc
+	!use omp_lib !!!
 	use type_vars
 	use consts
 	use read_pars
@@ -30,8 +31,8 @@ program saepot_calc
 	call pre_calc_dens()
 	
 !-- calc electron density
-!~  	call calc_dens_gau()  ! from Gaussian
- 	call calc_dens_tise() ! from TISE wf
+ 	call calc_dens_gau()  ! from Gaussian
+!~  	call calc_dens_tise() ! from TISE wf
 
 	call read_edens()     ! read eden, seden, gr_seden, ge_eden   
 	
@@ -124,6 +125,7 @@ function harpot
 
 	temp = 0.0_dp
 
+!~ 	!$omp parallel do private (i,j,k) reduction (+:temp)
 	do i = 1, Nrad
 		Rp = rad(i) ! r'
 		do j = 1, Neta
@@ -143,6 +145,7 @@ function harpot
 			enddo
 		enddo
 	enddo
+!~ 	!$omp end parallel do
 
 	harpot = temp	
 
@@ -183,9 +186,12 @@ end function
 !-----------
 
 function corpot(i,j)
+!-- Vosko et al. 1980
     integer, intent(in) :: i, j
     real(dp) :: corpot
-    real(dp), parameter :: a = 0.0621814/2
+
+!-- Using ferromagnetic parameters
+    real(dp), parameter :: a = 0.0621814/2 
     real(dp), parameter :: b = 7.06042
     real(dp), parameter :: c = 18.057
     real(dp), parameter :: x0 = -0.32500
@@ -202,8 +208,9 @@ function corpot(i,j)
     Xx0 = x0**2 + b*x0 + c
     Q = sqrt(4*c - b**2)
 
+!-- In Hartree unit, the formula (in Rydberg unit) must be added factor 1/2
     ec = 0.5 * a * ( log(x**2 / Xx) + 2*b/Q * atan(Q / (2*x + b) ) &
-                   - b*x0 / Xx0 * (log( (x-x0)**2 / Xx ) + 2*(b + x*x0) /Q *atan( Q/(2*x + b) ) ) )
+                   - b*x0 / Xx0 * (log( (x-x0)**2 / Xx ) + 2*(b + 2*x0) /Q *atan( Q/(2*x + b) ) ) )
 
     corpot = ec - a/6 * ( (x - x0)*c - b*x*x0 ) / ( (x - x0) * Xx )
    
